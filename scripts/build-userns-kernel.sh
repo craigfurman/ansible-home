f95ae7b137a3f006c3610791b2fa25e24810e3da
Craig Furman <craig.furman89@gmail.com>
2017-12-03 13:09:26 +0000

    Fix (and rename) build-userns-kernel.sh

--- scripts/build-userns-kernel.sh
+++ scripts/build-userns-kernel.sh
@@ -12,7 +12,7 @@ asp checkout linux
 (
 cd linux/repos/core-x86_64
 sed -i s/pkgbase=linux/pkgbase=linux-userns/g PKGBUILD
-sed -i 's/#\sCONFIG_USER_NS.*$/CONFIG_USER_NS=y/g' config.x86_64
+sed -i 's/#\sCONFIG_USER_NS.*$/CONFIG_USER_NS=y/g' config
 updpkgsums
 makepkg -s
 )
