fa40075a55aabfbd49d9a671a6c95b3c8b9b4316
Craig Furman <craig.furman89@gmail.com>
2018-12-15 18:26:21 +0000

    Local DNS names in pi-hole's dnsmasq
    craig-desktop and tiny-rick, for now

--- raspberry-pi/provision.sh
+++ raspberry-pi/provision.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 # Create ~/ansible_vault_password if it doesn't already exist from the lastpass entry
-ansible-playbook -i tiny-rick.local, --vault-password-file ~/ansible_vault_password \
+ansible-playbook -i 192.168.0.105, --vault-password-file ~/ansible_vault_password \
   "$(dirname "$0")/ansible-playbook.yml"
