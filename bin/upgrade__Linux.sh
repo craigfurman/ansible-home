d0525c8f0fe88bfc3dde8b336674bb3e9d77e555
Craig Furman <craig.furman89@gmail.com>
2019-08-12 11:50:01 +0100

    Get rid of AUR_BUILDER logic
    Only 1 Arch PC now.

--- bin/upgrade__Linux.sh
+++ bin/upgrade__Linux.sh
@@ -7,13 +7,8 @@ source "$(dirname "$0")/../shell_helpers/shell-helpers.sh"
 log "Updating pacman packages"
 sudo pacman -Syu --noconfirm
 
-# Usually, desktop builds AUR packages, laptop just pulls them from Google Drive
-if [ -n "${AUR_BUILDER:-}" ]; then
-  log "Updating AUR packages"
-  "$(dirname "$0")/aur-upgrade.sh"
-else
-  log "AUR_BUILDER not set: will not build updated AUR packages on this machine"
-fi
+log "Updating AUR packages"
+"$(dirname "$0")/aur-upgrade.sh"
 
 log "Cleaning pacman cache"
 sudo pacman -Sc --noconfirm
