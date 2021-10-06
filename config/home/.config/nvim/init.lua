5b15f23571fe6a34e7a5278f5cf63948f5d9df2a
Craig Furman <craig.furman89@gmail.com>
2021-10-06 19:04:30 +0100

    Telescope all the things faster

--- config/home/.config/nvim/init.lua
+++ config/home/.config/nvim/init.lua
@@ -203,6 +203,7 @@ nmap('<leader>ff', '<cmd>Telescope find_files<cr>')
 nmap('<leader>fg', '<cmd>Telescope live_grep<cr>')
 nmap('<leader>fb', '<cmd>Telescope buffers<cr>')
 nmap('<leader>fm', '<cmd>Telescope marks<cr>')
+nmap('<leader>fo', ':Telescope ', {noremap=true}) -- find other, not silent as prompt
 
 -- File tree
 require'nvim-tree'.setup({
