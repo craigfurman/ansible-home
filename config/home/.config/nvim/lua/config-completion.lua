b991ba85648eb10c1654fa79cb4e559066a87c6a
Craig Furman <craig.furman89@gmail.com>
2022-01-14 11:42:21 +0000

    Rm vestigial completion-nvim config

--- config/home/.config/nvim/lua/config-completion.lua
+++ config/home/.config/nvim/lua/config-completion.lua
@@ -1,19 +1,5 @@
 opt.completeopt = 'menuone,noinsert,noselect'
 opt.shortmess = opt.shortmess + 'c'
-g.completion_matching_smart_case = 1
-g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy', 'all'}
-g.completion_sorting = 'length'
-g.completion_auto_change_source = 1
-
--- g.completion_chain_complete_list = {
---   default = {
---     {complete_items = {'lsp'}},
---     {complete_items = {'snippet', 'buffers', 'tmux'}},
---     {mode = '<c-p>'},
---     {mode = '<c-n>'}
---   },
---   TelescopePrompt = {},
--- }
 
 -- Setup nvim-cmp.
 local cmp = require'cmp'
