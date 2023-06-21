185ac2d17efe45e6ae2596f81dc5d5989e2b30d8
Craig Furman <craig.furman89@gmail.com>
2023-06-21 22:27:14 +0100

    New file headers when files created in nvim-tree

--- config/home/.config/nvim/lua/headers.lua
+++ config/home/.config/nvim/lua/headers.lua
@@ -1,4 +1,11 @@
 local fileTemplate = os.getenv('NVIM_NEW_FILE_TEMPLATE')
 if fileTemplate ~= nil then
-  cmd('au BufNewFile * 0r '..fileTemplate)
+  cmd('au BufNewFile * 0read '..fileTemplate)
+
+  local api = require("nvim-tree.api")
+  local Event = api.events.Event
+  api.events.subscribe(Event.FileCreated, function(data)
+    cmd('edit '..data.fname)
+    cmd('0read '..fileTemplate)
+  end)
 end
